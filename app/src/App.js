072ba4d290a7aa59fd36287c5f2e46f24532f403
aswijapasupuleti <aswija.pasupuleti@gmail.com>
2024-06-19 15:23:55 +0530

    navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hellooo</h1>
-     <h2>aswija</h2>
+    <div>
+      <Navbar />
     </div>
   );
 }
